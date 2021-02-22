e91f5f325ca68b890ed1d24f36915ad4a111e49a
Ecassanm <78620296+Ecassanm@users.noreply.github.com>
2021-02-22 01:12:29 -0500

    Update software_sales.py

--- software_sales.py
+++ software_sales.py
@@ -7,28 +7,28 @@
 
 quan = int(input("Please input the number of packages to be purchased: "))
 if quan <= 0:
-	print("  Invalid Input!")
+    print("Invalid Input!")
 elif ((quan > 0) and (quan < 10)):
-	print("  No discount applied.")
-	total = quan * 99
-	print("  The final price for purchasing %1d packages is $%10,.2f" % (quan, total))
+    print("No discount applied.")
+        total = quan * 99
+            print("The final price for purchasing", quan, "packages is ${:.,2f}".format(total))
 elif ((quan >= 10) and (quan < 20)):
-	print("  10% discount applied.")
-	total = quan * 99
-	total = total * 0.9
-	print("  The final price for purchasing %1d packages is $%10,.2f" % (quan, total))
+    print("10% discount applied.")
+        total = quan * 99
+        total = total * 0.9
+            print("The final price for purchasing", quan, "packages is ${:.,2f}".format(total))
 elif ((quan >= 20) and (quan < 50)):
-	print("  25% discount applied.")
-	total = quan * 99
-	total = total * 0.75
-	print("  The final price for purchasing %1d packages is $%10,.2f" % (quan, total))
+    print("25% discount applied.")
+        total = quan * 99
+        total = total * 0.75
+            print("The final price for purchasing", quan, "packages is ${:.,2f}".format(total))
 elif ((quan >= 50) and (quan < 100)):
-	print("  35% discount applied.")
-	total = quan * 99
-	total = total * 0.65
-	print("  The final price for purchasing %1d packages is $%10,.2f" % (quan, total))
+    print("35% discount applied.")
+        total = quan * 99
+        total = total * 0.65
+            print("The final price for purchasing", quan, "packages is ${:.,2f}".format(total))
 else:
-	print("  45% discount applied.")
-	total = quan * 99
-	total = total * 0.55
-	print("  The final price for purchasing %1d packages is $%10,.2f" % (quan, total))
+    print("45% discount applied.")
+        total = quan * 99
+        total = total * 0.55
+            print("The final price for purchasing", quan, "packages is ${:.,2f}".format(total))
