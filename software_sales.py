dbb266926de29378b7351a613d4c6819b15f39de
Ecassanm <78620296+Ecassanm@users.noreply.github.com>
2021-02-22 00:05:40 -0500

    Add files via upload

---
+++ software_sales.py
@@ -0,0 +1,27 @@
+quan = input("Please input the number of packages to be purchased: ")
+if quan <= 0:
+	print("Invalid Input!")
+elif ((quan > 0) and (quan < 10)):
+	print("No discount applied.")
+	total = quan * 99
+	print("The final price for purchasing %1d packages is $%3.2f" % (quan, total))
+elif ((quan >= 10) and (quan < 20)):
+	print("10% discount applied.")
+	total = quan * 99
+	total = total * 0.9
+	print("The final price for purchasing %1d packages is $%3.2f" % (quan, total))
+elif ((quan >= 20) and (quan < 50)):
+	print("25% discount applied.")
+	total = quan * 99
+	total = total * 0.75
+	print("The final price for purchasing %1d packages is $%3.2f" % (quan, total))
+elif ((quan >= 50) and (quan < 100)):
+	print("35% discount applied.")
+	total = quan * 99
+	total = total * 0.65
+	print("The final price for purchasing %1d packages is $%3.2f" % (quan, total))
+else:
+	print("45% discount applied.")
+	total = quan * 99
+	total = total * 0.55
+	print("The final price for purchasing %1d packages is $%3.2f" % (quan, total))
